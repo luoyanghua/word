6918c503cd7da937915567bf141776ab940304e4
luoyanghua <1543888360@qq.com>
2018-11-13 21:45:25 +0800

    下班

---
+++ 王毅发的/标注/基于钉钉会诊/Script/InstantConsult/index.js
@@ -0,0 +1,12 @@
+var vm = new Vue({
+    el:'#app',
+    data:{
+        searchItemShow:0,//1-会诊医生 2-会诊状态
+    },
+    methods:{},
+    mounted:function () { 
+        this.$nextTick(function () {
+
+        });
+    }
+});
